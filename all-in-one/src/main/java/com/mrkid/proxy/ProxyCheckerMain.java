845ba9c10fe1cf3e202fec8ec7f81e6fa2b42857
wuxudong <wuxudong@wandoujia.comxudong82113@gmail.com>
2016-11-15 15:44:11 +0800

    only output valid proxy

--- all-in-one/src/main/java/com/mrkid/proxy/ProxyCheckerMain.java
+++ all-in-one/src/main/java/com/mrkid/proxy/ProxyCheckerMain.java
@@ -3,8 +3,8 @@ package com.mrkid.proxy;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mrkid.proxy.dto.Proxy;
+import com.mrkid.proxy.dto.ProxyCheckResponse;
 import com.mrkid.proxy.scheduler.ProxyChecker;
-import com.mrkid.proxy.utils.AddressUtils;
 import edu.uci.ics.crawler4j.crawler.CrawlController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,6 +16,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
+import java.util.stream.Collectors;
 
 /**
  * User: xudong
@@ -73,19 +74,23 @@ public class ProxyCheckerMain {
             final ObjectMapper objectMapper = new ObjectMapper();
             try (final PrintWriter jsonWriter = new PrintWriter(new FileWriter(allProxyInJsonFormat));
                  final PrintWriter squidWriter = new PrintWriter(new FileWriter(highAnonymityInSquidFormat))) {
-                proxyChecker.check(ip, proxyCheckUrl, proxies)
-                        .stream().forEach(line -> {
+                final List<ProxyCheckResponse> list = proxyChecker.check(ip, proxyCheckUrl, proxies);
+                list.forEach(line -> {
                     try {
                         jsonWriter.println(objectMapper.writeValueAsString(line));
-                        if ("http".equalsIgnoreCase(line.getProxy().getSchema())) {
-                            squidWriter.println(
-                                    String.format("cache_peer %s parent %d 0 round-robin no-query",
-                                            line.getProxy().getHost(), line.getProxy().getPort()));
-
-                        }
                     } catch (JsonProcessingException e) {
                     }
                 });
+
+                list.stream().filter(r -> r.isValid())
+                        .filter(line -> "http".equalsIgnoreCase(line.getProxy().getSchema()))
+                        .collect(Collectors.toSet())
+                        .forEach(line ->
+                                squidWriter.println(
+                                        String.format("cache_peer %s parent %d 0 round-robin no-query", line.getProxy
+                                                ().getHost(), line.getProxy().getPort())));
+
+
             }
 
             context.close();

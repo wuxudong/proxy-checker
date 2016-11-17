0649335c14cd305f230ef3c97fd48b6444a30a61
wuxudong <wuxudong@wandoujia.comxudong82113@gmail.com>
2016-11-17 13:52:27 +0800

    fix sh

--- all-in-one.sh
+++ all-in-one.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+#this script should be run as root
+
 java -DisEc2=true -jar all-in-one/target/all-in-one-1.0-SNAPSHOT.jar
 
-sudo su
 #remove all cache_peer
 
 sed -i.bak '/cache_peer/d' /etc/squid/squid.conf

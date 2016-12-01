6258a491a3b2a21401940b0ed1f501b47e2c0bd4
wuxudong <wuxudong@wandoujia.comxudong82113@gmail.com>
2016-12-01 15:16:03 +0800

    修改 all-in-one.sh

--- all-in-one.sh
+++ all-in-one.sh
@@ -2,10 +2,11 @@
 
 #this script should be run as root
 
-java -DisEc2=true -jar all-in-one/target/all-in-one-1.0-SNAPSHOT.jar
+ip=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+
+java -jar proxy-checker-client/target/proxy-checker-client-1.0-SNAPSHOT.jar -s http://${ip}:8080/proxy-check -a all
 
 #remove all cache_peer
-
 sed -i.bak '/cache_peer/d' /etc/squid/squid.conf
 
 cat data/high_anonymity_proxy.squid >> /etc/squid/squid.conf

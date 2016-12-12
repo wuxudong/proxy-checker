293c336a387488d63e3cd9fed3bc91d420e7210e
wuxudong <wuxudong@wandoujia.comxudong82113@gmail.com>
2016-12-12 20:56:16 +0800

    update shell

--- all-in-one.sh
+++ all-in-one.sh
@@ -2,17 +2,14 @@
 
 #this script should be run as root
 
-#my machine is running on ec2
-ip=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
-
-java -jar proxy-checker-client/target/proxy-checker-client-1.0-SNAPSHOT.jar -s http://${ip}:8080/proxy-check -a all
+java -jar proxy-checker-client/target/proxy-checker-client-1.0-SNAPSHOT.jar -a all
 
 #remove all cache_peer
-sed -i.bak '/cache_peer/d' /etc/squid/squid.conf
+sed -i.bak '/cache_peer/d' /etc/squid3/squid.conf
 
-cat data/proxy.squid | grep HIGH_ANONYMITY | sed 's/#HIGH_ANONYMITY.*//' >> /etc/squid/squid.conf
+cat data/proxy.squid | grep HIGH_ANONYMITY | sed 's/#HIGH_ANONYMITY.*//' >> /etc/squid3/squid.conf
 
-squid -k reconfigure
+squid3 -k reconfigure
 
 
 
